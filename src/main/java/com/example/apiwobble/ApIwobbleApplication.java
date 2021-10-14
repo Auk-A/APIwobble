bfedcfc12e053cc74b5b02962f52941d5656723e
[auke <[auke.aardoom@gmail.com]>
2021-10-14 14:03:26 +0200

    Initial commit, boilerplate code

---
+++ src/main/java/com/example/apiwobble/ApIwobbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiwobble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIwobbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApIwobbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiwobble/ApIwobbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiwobble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIwobbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
