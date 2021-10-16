09c3d269d974e67a4ab6e3bbec33335032eb7ec2
[auke <[auke.aardoom@gmail.com]>
2021-10-16 12:33:23 +0200

    Added user and car controllers

--- src/main/java/avans/apiwobble/domain/Car.java
+++ src/main/java/avans/apiwobble/domain/Car.java
@@ -1,4 +1,28 @@
 package avans.apiwobble.domain;
 
+import java.util.Date;
+
 public class Car {
+    private String carBrand;
+    private String carModel;
+    private String carColor;
+    private int carTopSpeed;
+    private String licensePlate;
+    private int carNewValue;
+    private int wltpRange;
+    private int carPower;
+    private double secondsTo100;
+    private Date carBuildDate;
+
+    public void getBuildDate(){
+
+    }
+
+    public void getInfo(){
+
+    }
+
+    public void addCar(){
+
+    }
 }
