8755059e355e2ec9672f431188fd1edb4850209d
Burak <b.bayraktar@student.avans.nl>
2021-10-19 21:41:41 +0200

    Trip and Wallet added

--- src/main/java/avans/apiwobble/domain/Trip.java
+++ src/main/java/avans/apiwobble/domain/Trip.java
@@ -1,4 +1,27 @@
 package avans.apiwobble.domain;
 
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.hibernate.dialect.MariaDB53Dialect;
+
 public class Trip {
+    private String userName;
+    private String licensePlate;
+    private String pointPickUp;
+    private String pointDropOff;
+    private int habitScore;
+    private int tripDuration;
+    private double tripPrice;
+    private MD5 parkingPhotos;
+
+    public void rentCar(int userName, String licensePlate){
+
+    }
+
+    public boolean uploadParkingPhotos(){
+
+    }
+
+    public void parkCar(){
+
+    }
 }
