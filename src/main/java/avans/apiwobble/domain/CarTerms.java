039fe05a6d6d070aab2bbc2c56ed75349d84c820
Burak <b.bayraktar@student.avans.nl>
2021-10-19 21:20:32 +0200

    CarTerms and User added

--- src/main/java/avans/apiwobble/domain/CarTerms.java
+++ src/main/java/avans/apiwobble/domain/CarTerms.java
@@ -1,4 +1,23 @@
 package avans.apiwobble.domain;
 
 public class CarTerms {
+    private int carTariff;
+    private String licensePlate;
+    private int carMileage;
+    private boolean rentedOut;
+    private int currentRange;
+    private String currentLocation;
+    private int currentValue;
+
+    public void calculateOwnershipCost(){
+        int ownerShipCost;
+    }
+
+    public void calculateUsageCost(){
+        int usageCost;
+    }
+
+    public void calculateSelfInsurance(){
+
+    }
 }
--- src/main/java/avans/apiwobble/domain/User.java
+++ src/main/java/avans/apiwobble/domain/User.java
@@ -1,4 +1,11 @@
 package avans.apiwobble.domain;
 
 public class User {
+    public String userName;
+    private String userMail;
+    public double userScore;
+
+    public void calculateScore(){
+
+    }
 }
