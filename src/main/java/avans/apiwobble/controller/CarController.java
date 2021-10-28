690335711cf83af166fe8727fae8a26ddf5295ce
Auk-A <auke.aardoom@gmail.com>
2021-10-28 13:45:58 +0200

    Fallback for date mapping (yyyy)

--- src/main/java/avans/apiwobble/controller/CarController.java
+++ src/main/java/avans/apiwobble/controller/CarController.java
@@ -45,8 +45,8 @@ public class CarController {
             @RequestParam(required = false, defaultValue = "2147483647") int max_top_speed,
             @RequestParam(required = false, defaultValue = "0") int min_value,
             @RequestParam(required = false, defaultValue = "2147483647") int max_value,
-            @RequestParam(required = false, defaultValue = "1900-01-01") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date min_build_date,
-            @RequestParam(required = false, defaultValue = "2100-01-01") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date max_build_date
+            @RequestParam(required = false, defaultValue = "1900-01-01") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, fallbackPatterns = { "yyyy" }) Date min_build_date,
+            @RequestParam(required = false, defaultValue = "2100-01-01") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, fallbackPatterns = { "yyyy" }) Date max_build_date
     ) {
         List<Car> found = new ArrayList<>(carRepository.findAll());
 
