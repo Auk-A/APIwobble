23befdbe2e3e5c2bed994a8e5bf49f87450361b8
Auk-A <auke.aardoom@gmail.com>
2021-11-03 21:18:39 +0100

    Added Model enum

---
+++ src/main/java/avans/apiwobble/domain/Model.java
@@ -0,0 +1,26 @@
+package avans.apiwobble.domain;
+
+public enum Model {
+    HATCHBACK,
+    SEDAN,
+    COUPE,
+    FASTBACK,
+    KAMMBACK,
+    NOTCHBACK,
+    SHOOTINGBRAKE,
+    BROUGHAM,
+    LANDAULET,
+    CROSSOVER,
+    SUV,
+    MPV,
+    STATIONWAGON,
+    LIMOUSINE,
+    SPIDER,
+    CABRIOLET,
+    TRUCK,
+    JEEP,
+    WEDGECAR,
+    TARGATOP,
+    MICROCAR
+
+}
